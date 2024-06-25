27830d7a605bc3ffc954fc6f69146ed9edd499fc
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-06-25 06:00:48 +0600

    fix: update release config to update version number on package.json

--- release.config.cjs
+++ release.config.cjs
@@ -3,14 +3,20 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false, // Disable npm publish as you're not publishing to npm
+      },
+    ],
     [
       '@semantic-release/git',
       {
-        assets: ['package.json'],
+        assets: ['package.json', 'CHANGELOG.md'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
-    '@semantic-release/changelog',
   ],
 };
 
